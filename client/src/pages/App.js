d84428c387f4a59ce9e509557df7c445b2cc9860
Chocoluna <cl.coutupetra@gmail.com>
2022-04-22 19:25:12 +0200

    first canva!

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,10 +1,7 @@
 import logo from '../assets/chocolate-bar.png';
-//CSS
-import {getTheme, setTheme} from '../theme';
-import '../css/App.css';
 import MenuAppBar from '../components/Header';
-import { Avatar, Button, CssBaseline, Grid, Paper } from '@material-ui/core';
-import { Column, Item, Row } from '@mui-treasury/components/flex';
+import { Stage, Layer, Star, Text, Rect } from 'react-konva';
+import '../css/App.css';
 
 
 var classes;
@@ -15,20 +12,20 @@ function App() {
   return (
       <div className="App">
         <MenuAppBar />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      
+      <Stage width={170} height={170}>
+        <Layer>
+        <Text text="Some text on canvas" fontSize={15} />
+          <Rect
+            x={20}
+            y={50}
+            width={100}
+            height={100}
+            fill="red"
+            shadowBlur={10}
+          />
+        </Layer>
+      </Stage>
       </div>
     
   );
